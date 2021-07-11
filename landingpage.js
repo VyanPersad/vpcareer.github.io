62a97912a5b55914419be08f314e66ed0fb761f8
Vyan <vyanpersad@gmail.com>
2021-07-10 22:11:02 -0400

    Added GitHub page

--- landingpage.js
+++ landingpage.js
@@ -1,32 +1,34 @@
 //Open
-function openacca() {accaview.style.visibility = "visible";}
-function openpet() {petview.style.visibility = "visible";}
-function openmech() {mechview.style.visibility = "visible";}
+function openacca() { accaview.style.visibility = "visible"; }
+function openpet() { petview.style.visibility = "visible"; }
+function openmech() { mechview.style.visibility = "visible"; }
 
-function openpad() {padview.style.visibility = "visible";}
-function openpapp() {appview.style.visibility = "visible";}
+function openpad() { padview.style.visibility = "visible"; }
+function openpapp() { appview.style.visibility = "visible"; }
 
-function spsl() {spslview.style.visibility = "visible";}
-function petro() {petroview.style.visibility = "visible";}
-function gulf() {gulfview.style.visibility = "visible";}
+function spsl() { spslview.style.visibility = "visible"; }
+function petro() { petroview.style.visibility = "visible"; }
+function gulf() { gulfview.style.visibility = "visible"; }
 
-function simeng() {blogview.style.visibility = "visible";}
-function gcad() {cadview.style.visibility = "visible";}
+function simeng() { blogview.style.visibility = "visible"; }
+function gcad() { cadview.style.visibility = "visible"; }
+function ghub() { hubview.style.visibility = "visible"; }
 
 //Close
-function closeacca() {accaview.style.visibility = "hidden";}
-function closepet() {petview.style.visibility = "hidden";}
-function closemech() {mechview.style.visibility = "hidden";}
+function closeacca() { accaview.style.visibility = "hidden"; }
+function closepet() { petview.style.visibility = "hidden"; }
+function closemech() { mechview.style.visibility = "hidden"; }
 
-function closepad() {padview.style.visibility = "hidden";}
-function closepapp() {appview.style.visibility = "hidden";}
+function closepad() { padview.style.visibility = "hidden"; }
+function closepapp() { appview.style.visibility = "hidden"; }
 
-function clspsl() {spslview.style.visibility = "hidden";}
-function clpetro() {petroview.style.visibility = "hidden";}
-function clgulf() {gulfview.style.visibility = "hidden";}
+function clspsl() { spslview.style.visibility = "hidden"; }
+function clpetro() { petroview.style.visibility = "hidden"; }
+function clgulf() { gulfview.style.visibility = "hidden"; }
 
-function clsimeng() {blogview.style.visibility = "hidden";}
-function clgcad() {cadview.style.visibility = "hidden";}
+function clsimeng() { blogview.style.visibility = "hidden"; }
+function clgcad() { cadview.style.visibility = "hidden"; }
+function clhub() { hubview.style.visibility = "hidden"; }
 
 window.addEventListener('click', outsideClick);
 function outsideClick(e) {
@@ -66,6 +68,9 @@ function outsideClick(e) {
   }
   if (e.target == cadview) {
     cadview.style.visibility = "hidden";
+  }
+  if (e.target == hubview) {
+    hubview.style.visibility = "hidden";
   }
 }
 
